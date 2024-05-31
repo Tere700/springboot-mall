2bd43c11952ec31d96d0a03d08fb54406ee11b4c
Ctere <tere06ccc@gmail.com>
2024-05-31 23:03:43 +0800

    商品功能 - 實作新增商品

--- springboot-mall/src/main/java/com/kujudy/springbootmall/service/ProductService.java
+++ springboot-mall/src/main/java/com/kujudy/springbootmall/service/ProductService.java
@@ -1,7 +1,9 @@
 package com.kujudy.springbootmall.service;
 
+import com.kujudy.springbootmall.dto.ProductRequest;
 import com.kujudy.springbootmall.model.Product;
 
 public interface ProductService {
     Product getProductById(Integer productId);
+    Integer createProduct(ProductRequest productRequest);
 }
