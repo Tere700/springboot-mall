c29c5a19b637662db5e650594250430e816f06c3
Ctere <tere06ccc@gmail.com>
2024-06-01 22:04:43 +0800

    商品功能 - 實作刪除商品

--- springboot-mall/src/main/java/com/kujudy/springbootmall/dao/impl/ProductDaoImpl.java
+++ springboot-mall/src/main/java/com/kujudy/springbootmall/dao/impl/ProductDaoImpl.java
@@ -82,4 +82,13 @@ public class ProductDaoImpl implements ProductDao {
         jdbcTemplate.update(sql, map);
 
     }
+
+    @Override
+    public void deleteProductById(Integer productId) {
+        String sql = "DELETE FROM product WHERE product_id = :productId";
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("productId", productId);
+        jdbcTemplate.update(sql, map);
+    }
 }
