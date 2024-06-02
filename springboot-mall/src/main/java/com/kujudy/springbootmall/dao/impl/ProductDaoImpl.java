cc510319eb394a2a8f078dd691b2d97f2781ecc1
Ctere <tere06ccc@gmail.com>
2024-06-02 11:20:59 +0800

    商品功能 - 實作查詢商品列表(基礎實作)

--- springboot-mall/src/main/java/com/kujudy/springbootmall/dao/impl/ProductDaoImpl.java
+++ springboot-mall/src/main/java/com/kujudy/springbootmall/dao/impl/ProductDaoImpl.java
@@ -23,6 +23,17 @@ public class ProductDaoImpl implements ProductDao {
     private NamedParameterJdbcTemplate jdbcTemplate;
 
     @Override
+    public List<Product> getProducts() {
+        String sql = "select product_id, product_name, category, image_url, price, stock, description, " +
+                "created_date, last_modified_date" +
+                " from product";
+
+        Map<String,Object> map = new HashMap<>();
+        List<Product> productList = jdbcTemplate.query(sql, map, new ProductRowMapper());
+        return productList;
+    }
+
+    @Override
     public Product getProductById(Integer productId) {
         String sql = "SELECT product.product_id, product_name, category, image_url, price, stock, description, " +
                 "created_date, last_modified_date " +
